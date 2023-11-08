69046f90e55ad369488cb2014a363307afc3bfea
Murat Varen <bebbobya@gmail.com>
2023-11-09 00:18:54 +0100

    COOKIE CLICKER-07: Xaml clean up, je kan nu pointers kopen voor passief inkomen, New methods voor: - buttons enabler - Dispatchertimer

--- Cookie_Clicker_Murat_Varen/MainWindow.xaml.cs
+++ Cookie_Clicker_Murat_Varen/MainWindow.xaml.cs
@@ -13,7 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Media.Animation;
-
+using System.Reflection;
+using System.Net.Security;
+using System.Windows.Threading;
 
 namespace Cookie_Clicker_Murat_Varen
 {
@@ -23,10 +25,24 @@ namespace Cookie_Clicker_Murat_Varen
     public partial class MainWindow : Window
     {
         private readonly MediaPlayer tapSoundPlayer = new MediaPlayer();
+        private DispatcherTimer klok = new DispatcherTimer();
         private decimal cookieCounter = 0;
+        private int pointerCounter = 0;
+        private const decimal pointerBasePrice = 15;
+        private decimal pointerPrice = 15;
         public MainWindow()
         {
             InitializeComponent();
+            klok.Tick += new EventHandler(KlokAfgelopen);
+            klok.Interval = new TimeSpan(0, 0, 0, 0, 10);
+            klok.Start();
+        }
+        private void KlokAfgelopen(object sender, EventArgs e)
+        {
+            PassiveIncome(pointerCounter, 0.001m);
+            LblCookieCount.Content = $"{cookieCounter.ToString("00.00")} cookies";
+            LblPassiveIncome.Content = $"+{pointerCounter * 0.01m}";
+            ButtonEnabler();
         }
 
         private void ImgCookie_MouseDown(object sender, MouseButtonEventArgs e)
@@ -40,10 +56,26 @@ namespace Cookie_Clicker_Murat_Varen
             ImgCookie.BeginAnimation(Image.MarginProperty, animation);
             RandomTapSound();
             cookieCounter++;
-            LblCookieCount.Content = $"{cookieCounter.ToString("00")} cookies";
+            LblCookieCount.Content = $"{cookieCounter.ToString("00.00")} cookies";
             this.Title = $"{cookieCounter}";
         }
 
+        public void ButtonEnabler()
+        {
+            if(cookieCounter >= pointerPrice)
+            {
+                BtnPointer.IsEnabled = true;
+            }
+            else
+            {
+                BtnPointer.IsEnabled = false;
+            }
+        }
+        public void PassiveIncome(int counter,decimal ammount)
+        {
+            cookieCounter += counter * ammount;
+
+        }
 
         public void RandomTapSound()
         {
@@ -63,6 +95,14 @@ namespace Cookie_Clicker_Murat_Varen
                 switch(btnContent)
                 {
                     case "Pointer":
+                        if(cookieCounter >= pointerPrice)
+                        {
+                            cookieCounter -= pointerPrice;
+                            pointerCounter++;
+                            LblPointerCounter.Content = pointerCounter;
+                            pointerPrice = Math.Round(pointerCounter * 1.15m * pointerBasePrice);
+                            LblPointerPrice.Content = pointerPrice;
+                        }
                         break;
                     case "Granny":
                         break;
