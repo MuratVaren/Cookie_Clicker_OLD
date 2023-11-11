038c6dff72faefe8b58cf38cbdef6a7057ca785c
Murat Varen <bebbobya@gmail.com>
2023-11-11 12:44:44 +0100

    COOKIE CLICKER-10: Tool tip voor upgrade buttons, Naming conventies fix, meer comments, Methode btnenabler => veel simpler via bool, idle Animatie voor koekie => 360 draait rond op zijn middenpunt, Nieuw animatie voor label cookie count, het groeit en verandert van kleur. en aantal nieuwe iconen

--- Cookie_Clicker_Murat_Varen/MainWindow.xaml.cs
+++ Cookie_Clicker_Murat_Varen/MainWindow.xaml.cs
@@ -26,6 +26,7 @@ namespace Cookie_Clicker_Murat_Varen
     {
         private readonly MediaPlayer tapSoundPlayer = new MediaPlayer();
         private readonly DispatcherTimer klok = new DispatcherTimer();
+
         private double cookieCounter = 0;
 
         private int pointerCounter = 0;
@@ -46,9 +47,11 @@ namespace Cookie_Clicker_Murat_Varen
         private double minePrice = 12000;
 
 
-        private int FactoryCounter = 0;
-        private const double FactoryBasePrice = 130000;
-        private double FactoryPrice = 130000;
+        private int factoryCounter = 0;
+        private const double factoryBasePrice = 130000;
+        private double factoryPrice = 130000;
+
+        
 
         public MainWindow()
         {
@@ -56,110 +59,134 @@ namespace Cookie_Clicker_Murat_Varen
             klok.Tick += new EventHandler(KlokAfgelopen);
             klok.Interval = new TimeSpan(0, 0, 0, 0, 10);
             klok.Start();
+            IdleAnimation();
+            ColorChanger();
         }
         private void KlokAfgelopen(object sender, EventArgs e)
         {
+            // passief inkomen updaten
             PassiveIncome(pointerCounter, 0.001);
             PassiveIncome(grannyCounter, 0.01);
             PassiveIncome(farmCounter, 0.08);
             PassiveIncome(mineCounter, 0.47);
-            PassiveIncome(FactoryCounter, 2.60);
+            PassiveIncome(factoryCounter, 2.60);
 
-
+            // labels en titel updaten
             LblCookieCount.Content = $"{cookieCounter.ToString("00.00")} cookies";
-            LblPassiveIncome.Content = $"+{(pointerCounter * 0.1m) + (grannyCounter * 1m) + (farmCounter * 8m) + (mineCounter * 47m) + (FactoryCounter * 260m)} ";
+            LblPassiveIncome.Content = $"+{(pointerCounter * 0.1) + (grannyCounter * 1) + (farmCounter * 8) + (mineCounter * 47) + (factoryCounter * 260)} ";
             this.Title = $"{cookieCounter.ToString("00")} cookies";
+
             ButtonEnabler();
         }
+        public void IdleAnimation()
+        {
+            // new rotatie object 
+            RotateTransform rotateTransform = new RotateTransform();
+            ImgCookie.RenderTransform = rotateTransform;
 
+            // rotatie via center van afbeelding
+            ImgCookie.RenderTransformOrigin = new Point(0.5, 0.5);
+
+            // simple animatie
+            DoubleAnimation rotateAnimation = new DoubleAnimation()
+            {
+                From = 0,
+                To = 360,
+                Duration = new Duration(TimeSpan.FromSeconds(5)),
+                RepeatBehavior = RepeatBehavior.Forever
+            };
+
+            // animatie spelen op de rotatie
+            rotateTransform.BeginAnimation(RotateTransform.AngleProperty, rotateAnimation);
+
+        }
         private void ImgCookie_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            // afbeelding aanpassen bij klik
             BitmapImage bitmapImage =
             new BitmapImage(new Uri("assets/Images/kookie2.png", UriKind.RelativeOrAbsolute));
             ImgCookie.Source = bitmapImage;
+
+            // simple animatie 
             ThicknessAnimation animation = new ThicknessAnimation
             {
-                
-                To = new Thickness(0),
+                To = new Thickness(15),
                 Duration = TimeSpan.FromMilliseconds(110),
                 FillBehavior = FillBehavior.Stop,
             };
-            animation.Completed += (s, _) =>
-            {
-                // Change the image source after the animation is complete
-                BitmapImage newImage = new BitmapImage(new Uri("assets/Images/cookietest.png", UriKind.RelativeOrAbsolute));
-                ImgCookie.Source = newImage;
-            };
+            // animatie einde == terug naar originele afbeelding
+            animation.Completed += Animation_Completed;
+
+            // animatie spelen op margin
             ImgCookie.BeginAnimation(Image.MarginProperty, animation);
+
             RandomTapSound();
             cookieCounter++;
             LblCookieCount.Content = $"{cookieCounter.ToString("00.00")} cookies";
         }
 
+        private void Animation_Completed(object sender, EventArgs e)
+        {
+            // originele afbeelding
+            BitmapImage newImage = new BitmapImage(new Uri("assets/Images/cookietest.png", UriKind.RelativeOrAbsolute));
+            ImgCookie.Source = newImage;
+        }
+
+        public void ColorChanger()
+        {
+            ColorAnimationUsingKeyFrames colorAnimation = new ColorAnimationUsingKeyFrames()
+            {
+                AutoReverse = true,
+                RepeatBehavior = RepeatBehavior.Forever,
+                Duration = TimeSpan.FromMilliseconds(1500)
+            };
+            colorAnimation.KeyFrames.Add(new LinearColorKeyFrame(Colors.Red, KeyTime.FromPercent(0.0)));
+            colorAnimation.KeyFrames.Add(new LinearColorKeyFrame(Colors.Blue, KeyTime.FromPercent(0.5)));
+            colorAnimation.KeyFrames.Add(new LinearColorKeyFrame(Colors.Green, KeyTime.FromPercent(1.0)));
+
+            LblCookieCount.Foreground = new SolidColorBrush(Colors.Red);
+            DoubleAnimation fontsizeAnimation = new DoubleAnimation()
+            {
+                From = 24,
+                To = 30,
+                Duration = TimeSpan.FromMilliseconds(300),
+                AutoReverse = true,
+                RepeatBehavior = RepeatBehavior.Forever,
+            };
+            LblCookieCount.Foreground.BeginAnimation(SolidColorBrush.ColorProperty, colorAnimation);
+            LblCookieCount.BeginAnimation(Label.FontSizeProperty, fontsizeAnimation);
+        }
         public void ButtonEnabler()
         {
-            if(cookieCounter >= pointerPrice)
-            {
-                BtnPointer.IsEnabled = true;
-            }
-            else
-            {
-                BtnPointer.IsEnabled = false;
-            }
-
-            if (cookieCounter >= grannyPrice)
-            {
-                BtnGranny.IsEnabled = true;
-            }
-            else
-            {
-                BtnGranny.IsEnabled = false;
-            }
-
-            if (cookieCounter >= farmPrice)
-            {
-                BtnFarm.IsEnabled = true;
-            }
-            else
-            {
-                BtnFarm.IsEnabled = false;
-            }
-
-            if (cookieCounter >= minePrice)
-            {
-                BtnMine.IsEnabled = true;
-            }
-            else
-            {
-                BtnMine.IsEnabled = false;
-            }
-
-            if (cookieCounter >= FactoryPrice)
-            {
-                BtnFactory.IsEnabled = true;
-            }
-            else
-            {
-                BtnFactory.IsEnabled = false;
-            }
+            // buttons disablen/enablen als je genoeg cookies hebt
+            BtnPointer.IsEnabled = cookieCounter >= pointerPrice;
+            BtnGranny.IsEnabled = cookieCounter >= grannyPrice;
+            BtnFarm.IsEnabled = cookieCounter >= farmPrice;
+            BtnMine.IsEnabled = cookieCounter >= minePrice;
+            BtnFactory.IsEnabled = cookieCounter >= factoryPrice;
         }
         public void PassiveIncome(int counter,double ammount)
         {
+            // simple methode dat cookie counter update
             cookieCounter += counter * ammount;
-
         }
 
         public void RandomTapSound()
         {
+            // geluid randomizer
             Random random = new Random();
             int number = random.Next(1, 3);
             tapSoundPlayer.Open(new Uri($"Assets/Audio/tap-{number}.wav", UriKind.RelativeOrAbsolute));
+
+            // geluid stoppen als het nog aan het spelen was
             tapSoundPlayer.Stop();
             tapSoundPlayer.Play();
         }
 
         private void BtnBuy_Click(object sender, RoutedEventArgs e)
         {
+            // een methode voor alle knoppen via sender
+            // cookiecounter updaten, labels updaten, upgrades++ en prijs updaten
             Button button = (Button)sender;
             if(button != null)
             {
@@ -211,13 +238,13 @@ namespace Cookie_Clicker_Murat_Varen
                         break;
 
                     case "Factory":
-                        if (cookieCounter >= FactoryPrice)
+                        if (cookieCounter >= factoryPrice)
                         {
-                            cookieCounter -= FactoryPrice;
-                            FactoryCounter++;
-                            LblFactoryCounter.Content = FactoryCounter;
-                            FactoryPrice = Math.Round(FactoryBasePrice * Math.Pow(1.15, FactoryCounter));
-                            LblFactoryPrice.Content = FactoryPrice;
+                            cookieCounter -= factoryPrice;
+                            factoryCounter++;
+                            LblFactoryCounter.Content = factoryCounter;
+                            factoryPrice = Math.Round(factoryBasePrice * Math.Pow(1.15, factoryCounter));
+                            LblFactoryPrice.Content = factoryPrice;
                         }
                         break;
                 }
